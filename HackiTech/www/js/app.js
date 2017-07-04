6df88f711d5ee7069ae1c83c3665f9cc029a2f68
Manjeet Singh Bargoti <msb.2905@gmail.com>
2017-07-04 13:44:53 +0530

    Added content by category

--- HackiTech/www/js/app.js
+++ HackiTech/www/js/app.js
@@ -30,7 +30,7 @@ angular.module('starter', ['ionic'])
     .state('main.contentRecent', {
       url: '/contentRecent',
       templateUrl: 'templates/menuContent.html',
-      controller: 'MenuCtrl'
+      controller: 'MainCtrl'
     })
     .state('main.postDetail', {
       url: '/postDetail/:postId',
--- HackiTech/www/js/controllers.js
+++ HackiTech/www/js/controllers.js
@@ -1,10 +1,131 @@
 angular.module('starter')
-	.controller('MenuCtrl', function(){
-		// alert('Hello!');
+
+	.controller('MenuCtrl', function($scope, $http, $sce, $ionicScrollDelegate){
+		
+		$scope.categories = [];
+
+		$http.get("https://hackitech.com/api/get_category_index/").then(
+			function(returnData){
+
+				$scope.categories = returnData.data.categories;
+				$scope.categories.forEach(function(element, index, array){
+					element.title = $sce.trustAsHtml(element.title);
+
+				})
+
+				console.log(returnData);
+
+			}, function(err) {
+
+				console.log(err);
+
+			})
 	})
 
-	.controller('PostCtrl', function(){
-		// alert('Hello!');
+
+	.controller('MainCtrl', function($scope, $http, $sce, $ionicScrollDelegate) {
+
+		$scope.offset = 0;
+		$scope.count_total = 1;
+
+		$scope.doRefresh = function()
+		{
+			$scope.recent_posts = [];
+
+			$http.get("https://hackitech.com/api/get_posts/").then(
+				function(returnPostData) {
+					console.log(returnPostData);
+					$scope.recent_posts = returnPostData.data.posts;
+					$scope.count_total = returnPostData.data.count_total;	
+
+					$scope.recent_posts.forEach(function(element, index, array){
+						element.excerpt = element.excerpt.substr(0,90);
+						element.excerpt = element.excerpt + "... Read More";
+						element.excerpt = $sce.trustAsHtml(element.excerpt);
+						element.title = $sce.trustAsHtml(element.title);
+					})
+
+					$scope.$broadcast('scroll.refreshComplete');
+
+				}, function(err) {
+
+			})
+		}
+
+		$scope.recent_posts = [];
+
+		$http.get("https://hackitech.com/api/get_posts/").then(
+			function(returnPostData) {
+				// console.log(returnPostData);
+				$scope.recent_posts = returnPostData.data.posts;
+				$scope.count_total = returnPostData.data.count_total;
+
+				$scope.recent_posts.forEach(function(element, index, array){
+					element.excerpt = element.excerpt.substr(0,90);
+					element.excerpt = element.excerpt + "... Read More";
+					element.excerpt = $sce.trustAsHtml(element.excerpt);
+					element.title = $sce.trustAsHtml(element.title);
+				})
+
+			}, function(err) {
+
+		});
+
+		$scope.canLoadMore = function()
+		{
+			return true;
+		};
+
+
+		$scope.loadMore = function()
+		{
+			
+
+			$http.get("https://hackitech.com/api/get_posts/?offset="+$scope.offset).then(
+				function(returnPostData) {
+					var newPosts = returnPostData.data.posts;
+					$scope.count_total = returnPostData.data.count_total;
+
+					newPosts.forEach(function(element, index, array){
+						element.excerpt = element.excerpt.substr(0,90);
+						element.excerpt = element.excerpt + "... Read More";
+						element.excerpt = $sce.trustAsHtml(element.excerpt);
+						element.title = $sce.trustAsHtml(element.title);
+					})
+
+					$scope.recent_posts.push.apply($scope.recent_posts, newPosts);
+					$scope.$broadcast("scroll.infiniteScrollComplete");
+					$scope.offset += 10;
+
+			});
+		};
+
+		$scope.searchTextChanged = function()
+		{
+			$ionicScrollDelegate.$getByHandle('mainScroll').scrollTop(true);
+		}
+	})
+
+	.controller('PostCtrl', function($scope, $http, $sce, $stateParams){
+		
+		$http.get('https://hackitech.com/api/get_post/?id='+ $stateParams.postId).then(
+			function(returnPost) {
+				$scope.post_title = returnPost.data.post.title;
+				$scope.post_category = returnPost.data.post.categories[0].title ? returnPost.data.post.categories[0].title : 'no category';
+				$scope.post_content = $sce.trustAsHtml(returnPost.data.post.content);
+				$scope.post_date = returnPost.data.post.date;
+				$scope.post_authorName = returnPost.data.post.author.first_name + " " + returnPost.data.post.author.last_name;
+					if($scope.post_authorName.trim() == '')
+						$scope.post_authorName = "Manjeet Bargoti";
+					$scope.post_authorImage = 'http://ionicframework.com/img/docs/mcfly.jpg';
+					$scope.post_image = returnPost.data.post.thumbnail_images.full.url;
+					$scope.post_commentCount = returnPost.data.post.comment_count;
+					$scope.post_views = returnPost.data.post.custom_fields.post_views_count[0];
+					$scope.post_url = returnPost.data.post.url;
+			}, function(err) {
+
+			})
+
 	})
 
 
